842946a4e25bc0cfae011b1190851a390a3ab47b
pablo179 <castillo.morales.pablo.prog@gmail.com>
2019-08-06 19:09:06 -0500

    first deploy

---
+++ src/index.js
@@ -0,0 +1,96 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const url = require("url");
+const path = require("path");
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/intro.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
+  mainWindow.maximize();
+  setTimeout(() => {
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "views/index.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
+  }, 5500);
+  // Menu
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+  // Set The Menu to the Main Window
+  Menu.setApplicationMenu(mainMenu);
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+const templateMenu = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Develop",
+        click() {
+          // Open the DevTools.
+          mainWindow.webContents.openDevTools();
+        }
+      },
+      {
+        label: "Exit",
+        accelerator: process.platform == "darwin" ? "command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
